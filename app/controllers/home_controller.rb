6680517b933a84eb756d2ff32a0e15ebcfd673ea
DIPANSHU SAJWAN <dsajwan98@gmail.com>
2019-06-25 02:12:06 +0530

    Login functionality

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,34 @@
+class HomeController < ApplicationController
+
+    def home
+
+    end
+
+    def contact
+    
+    end
+
+    def login
+
+    end
+
+    def auth
+        
+        @email=params[:user][:email]
+        @password=params[:user][:password]
+
+        @email_db="example@example.com"
+        @password_db="abc123" 
+        if (@email==@email_db) && (@password==@password_db)
+           flash[:success]="Logged IN"
+           redirect_to '/home'
+           
+        else
+            flash[:danger]="Wrong credentials"
+            render :action => :login
+        end
+        
+    end
+
+    
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  
+   root to:'home#index'
+   get '/home', to:'home#index'
+   get '/login' ,to:'home#login'
+   get '/contact', to:'home#contact'
+   post '/auth', to:'home#auth'
+
+end
